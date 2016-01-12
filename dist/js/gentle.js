3e694c1697cb679b1587e7876fc318629e6dc406
thiamdossantos <thia.mdossantos@gmail.com>
2016-01-11 22:25:46 -0200

    working on js

--- dist/js/gentle.js
+++ dist/js/gentle.js
@@ -22,16 +22,19 @@ var btnsRipple = function (e) {
 
 document.addEventListener('click', btnsRipple, false);
 
-var materialForm = function() {
-  if (this.value) {
-    this.classList.add('used');
-  } else{
-    this.classList.remove('used');
-  };
-};
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.form-group input[type=text], .form-group input[type=email], .form-group input[type=number], .form-group input[type=password], .form-group input[type=search], .form-group input[type=tel], .form-group input[type=url], .form-group textarea').addEventListener('blur', materialForm, false);
-});
+// if (document.readyState === 'complete') {
+//   function () {
+//     var formEl = document.querySelectorAll('.form-group input[type=text], .form-group input[type=email], .form-group input[type=number], .form-group input[type=password], .form-group input[type=search], .form-group input[type=tel], .form-group input[type=url], .form-group textarea');
+//     for (i = 0; i < formEl.length; i++) {
+//       document.formEl[i].onblur = function() {
+//         if (formEl[i].value) {
+//           formEl[i].classList.add('used');
+//         } else{
+//           formEl[i].classList.remove('used');
+//         };
+//       };
+//     };
+//   });
+// }; 
 
 //# sourceMappingURL=gentle.js.map
