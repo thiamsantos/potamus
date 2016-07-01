b1e705d2490e1d6117ae951f1733ace740f2333d
thiamsantos <thia.mdossantos@gmail.com>
2016-07-01 19:26:02 -0300

    adding form component

--- components/button/button.js
+++ components/button/button.js
@@ -1,9 +1,9 @@
-var btnsRipple = function (e) {
-  var target = e.target;
-  var btnClass = target.className.toLowerCase();
-  if(btnClass !== 'btn' && btnClass !== 'btn-small' && btnClass !== 'btn-large' && btnClass !== 'btn-block') return false;
-  var rect = target.getBoundingClientRect();
-  var ripple = target.querySelector('.ripple');
+{
+let btnsRipple = e => {
+  let target = e.target;
+  if(!target.classList.contains('c-btn')) return false;
+  let rect = target.getBoundingClientRect();
+  let ripple = target.querySelector('.ripple');
   if (!ripple) {
     ripple = document.createElement('span');
     ripple.className = 'ripple';
@@ -13,8 +13,8 @@ var btnsRipple = function (e) {
   }
 
   ripple.classList.remove('show');
-  var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-  var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+  let top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+  let left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
   ripple.style.top = top + 'px';
   ripple.style.left = left + 'px';
   ripple.classList.add('show');
@@ -22,3 +22,4 @@ var btnsRipple = function (e) {
 };
 
 document.addEventListener('click', btnsRipple, false);
+}
